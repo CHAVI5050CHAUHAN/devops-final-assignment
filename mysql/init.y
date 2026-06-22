bf9884c0641eb125d9f7bf3d579ac86972089198
Hariom_Chauhan <chavi5050chauhan@gmail.com>
2026-06-22 19:41:31 +0000

    Initial project structure

---
+++ mysql/init.y
@@ -0,0 +1,9 @@
+CREATE TABLE employees(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100)
+);
+
+INSERT INTO employees(name)
+VALUES
+('Hariom'),
+('DevOps Engineer');
---
+++ php/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<h1>DevOps Final Assignment</h1>";
+echo "<h3>Nginx + PHP-FPM + MySQL Working Successfully</h3>";
+echo "<p>Hostname: " . gethostname() . "</p>";
+
+?>
